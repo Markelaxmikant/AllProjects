dc659fd1452719c528f038c58c401ba43fd13021
laxmikantmarke <markelaxmikant99@gmail.com>
2024-01-15 17:49:18 +0530

    gen

--- genrandomclr/src/components/GenRandmClr.jsx
+++ genrandomclr/src/components/GenRandmClr.jsx
@@ -1,20 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const GenRandmClr = () => {
-    const [colorType, setColorType] = useState("hex")
-    const [color, setColor] = useState("#000")
+  const [colorType, setColorType] = useState("hex");
+  const [color, setColor] = useState("#000");
+
+  function randomColorUtility(length){
+      return Math.floor(Math.random()*length)
+  }
+  function handleRamdomHexColor() {
+    // #125466
+
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+    let hexColor = "#";
+
+    for (let i=0; i<6; i++){
+        hexColor += hex[randomColorUtility(hex.length)]
+    }
+    console.log(hexColor)
+    setColor(hexColor)
+  }
+  function handleRamdomRgbColor() {
+    const r = randomColorUtility(256)
+    const g = randomColorUtility(256)
+    const b = randomColorUtility(256)
+
+    setColor(`rgb(${r},${g},${b})`)
+    console.log(`rgb(${r},${g},${b})`)
+
+
+  }
   return (
-    <div style={{
+    <div
+      style={{
         height: "100vh",
         width: "100vw",
-        background:color
-    }}>
-        <h1>Genrate Random Color</h1>
-        <button>Gen HEX Color</button>
-        <button>Gen RGBColor</button>
-        <button>Gen Ramdom Color</button>
+        background: color,
+      }}
+    >
+      <h1>Genrate Random Color</h1>
+      <button onClick={() => setColorType("hex")}>Gen HEX Color</button>
+      <button onClick={() => setColorType("rgb")}>Gen RGBColor</button>
+      <button
+        onClick={
+          colorType === "hex" ? handleRamdomHexColor : handleRamdomRgbColor
+        }
+      >
+        Gen Ramdom Color
+      </button>
+      <div style={{
+        display: 'flex',
+        justifyContent : "center",
+        alignItems : "center",
+        color:"#fff",
+        fontSize:"50px",
+        marginTop:"50px",
+        flexDirection:"column",
+        gap:'20px'
+      }}>
+        <h2>{colorType === "rgb" ? "RGB Color" : "HEX Color"}</h2>
+        <h2>{color}</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GenRandmClr
+export default GenRandmClr;
